b41a53ecd6ed4d47f29603ba1281df2b8d8c8412
parijatmitra <parijatmitra20@gmail.com>
2022-02-05 23:59:23 +0530

    dark light mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
   
    <Navbar title="TextUtils" aboutText="About"/>
-   <div className="container my-3">
+   {/* <div className="container my-3">
    <TextForm heading="Enter Text here" />
-   </div>
+   </div> */}
+  <div className="container">
+  <About />
+  </div>
+  
    
    
     </>
